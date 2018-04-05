2b8f1d65ec69b0c0b066a4d652831778da6002d9
laure peucelle <laurepeu@hotmail.com>
2018-04-05 19:28:07 +0200

    add a header.css layout, fix indent error, add section class and rename class

--- config.rb
+++ config.rb
@@ -60,8 +60,8 @@ end
 # Build-specific configuration
 # https://middlemanapp.com/advanced/configuration/#environment-specific-settings
 
-# configure :build do
-#   activate :minify_css
-#   activate :minify_javascript
-# end
+configure :build do
+  activate :minify_css
+  activate :minify_javascript
+end
 
